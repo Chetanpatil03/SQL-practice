546227b06ceabb8ad17f85933cc1b054defe6b95
Chetan Patil <chetanpatil6303@gmail.com>
2025-05-06 14:51:03 +0530

    initial commit

--- demo.sql
+++ demo.sql
@@ -13,3 +13,8 @@ INSERT INTO Student (StudentID, FirstName, LastName, DateOfBirth, Gender, Email,
 VALUES 
 (1, 'John', 'Doe', '2000-01-15', 'M', 'john.doe@example.com', '1234567890', '2023-09-01'),
 (2, 'Jane', 'Smith', '1999-05-22', 'F', 'jane.smith@example.com', '0987654321', '2023-09-01');
+
+UPDATE Student
+SET FirstName = 'Ravi'
+WHERE StudentID = 1;
+
