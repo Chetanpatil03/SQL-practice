f8b374a13a375031f7f74f97cae70ef510a83b64
Chetan Patil <chetanpatil6303@gmail.com>
2025-05-08 18:11:13 +0530

    question number 3

--- Query4_.sql
+++ Query4_.sql
@@ -15,7 +15,11 @@ INSERT INTO students (rollno, name, marks) VALUES (101, 'anil', 78),
 (105, 'emanuel', 92),
 (106, 'farah', 82);
 
+alter table students add column city varchar(20) default "mumbai";
+
 select * from students;
+
+update students set city = "Delhi" where rollno in (104,105,106);
 
 
 -- SubQueries : nested/inner queries
@@ -65,12 +69,38 @@ WHERE
 
 -- combined way (dynamic )
 
-select rollno,name
-from students
-where rollno in (select rollno from students where rollno % 2 = 0);
+SELECT 
+    rollno, name
+FROM
+    students
+WHERE
+    rollno IN (SELECT 
+            rollno
+        FROM
+            students
+        WHERE
+            rollno % 2 = 0);
+            
+-- Que 3 : find the max marks from students of delhi
 
+-- my way : 
 
-   
+select name,rollno from students where city = "Delhi" and marks in (select max(marks) from students);
+
+-- her way : 
+	-- step 1 :
+    select rollno from students where city = "Delhi"; 
+	
+    -- step 2 : 
+    select max(marks) from students where rollno in (104,105,106);
+    
+    
+-- best way / subquery in from part
+
+select max(marks) from (select * from students where city = "Delhi") as temp;
+-- alice is must
+    
+    
 
 
 
