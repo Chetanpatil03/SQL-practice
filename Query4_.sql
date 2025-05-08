287641a80b6cea7887b11d55f8df64dcc8b75bba
Chetan Patil <chetanpatil6303@gmail.com>
2025-05-08 17:54:00 +0530

    question two for subquery

--- Query4_.sql
+++ Query4_.sql
@@ -37,6 +37,42 @@ select name,marks from students where marks >  87.6667;
 -- combined way (dynamic way ) : if it increases or decreases avgerage it doesn't affect
 select name,marks from students where marks > (select avg(marks) from students);
 
+-- Que 2 : find the students who have even roll numbers
+
+-- my way :  
+
+select rollno,name from students where rollno % 2 = 0;  -- even numbers 
+select rollno,name from students where not rollno % 2 = 0;  -- odd numberes
+
+-- her way :
+-- 1 step :
+SELECT 
+    rollno
+FROM
+    student
+WHERE
+    rollno % 2 = 0;
+    
+-- step 2 :
+
+SELECT 
+    rollno, name
+FROM
+    students
+WHERE
+    rollno IN (102 , 104, 106);
+
+
+-- combined way (dynamic )
+
+select rollno,name
+from students
+where rollno in (select rollno from students where rollno % 2 = 0);
+
+
+   
+
+
 
 
 
